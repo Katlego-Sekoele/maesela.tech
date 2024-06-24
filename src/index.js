691ef7ba4804ed6fa6dabe3921568874e3078fd3
Katlego-Sekoele <sekoelekatlego@gmail.com>
2024-06-24 19:17:34 +0200

    added vercel speed insights

--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Analytics } from '@vercel/analytics/react';
-import { inject } from '@vercel/analytics';
-
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Analytics/>
+    <Analytics />
+    <SpeedInsights/>
     <BrowserRouter>
        <App />
     </BrowserRouter>
